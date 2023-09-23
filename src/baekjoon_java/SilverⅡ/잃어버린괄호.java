2f5ef146478406d44ff0171e6b0c684851d339db
jung <wldud2550@naver.com>
2023-09-23 10:10:05 +0900

    [BOJ] 잃어버린괄호 / 실버2 / 20분(fail)

---
+++ src/baekjoon_java/SilverⅡ/잃어버린괄호.java
@@ -0,0 +1,21 @@
+package baekjoon_java.SilverⅡ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 잃어버린괄호 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+
+        while(st.hasMoreTokens()) {// true / false
+            StringTokenizer str = new StringTokenizer(st.nextToken(),"+");
+
+
+        }
+    }
+}
