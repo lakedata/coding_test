c9bcd3dfa8dbc3f4896e27093dc15edb6273f196
jung <wldud2550@naver.com>
2023-09-23 09:41:11 +0900

    [BOJ] ATM / 실버1 / 30분

---
+++ src/baekjoon_java/SilverIV/ATM.java
@@ -0,0 +1,42 @@
+package baekjoon_java.SilverIV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ATM {
+    public static void main(String[] arg) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int atm[] = new int[N + 1];
+
+        for (int i = 0; i < N; i++) {
+            atm[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(atm);
+
+        int sum = 0;
+        int p[] = new int[N + 1];
+
+        for (int i = 0; i < atm.length; i++) {
+            if (i == 0)
+                p[i] += atm[i];
+            else
+                p[i] += p[i - 1] + atm[i];
+
+        }
+
+        for (int i = 0; i < atm.length; i++) {
+            sum += p[i];
+        }
+
+        System.out.println(sum);//각 사람이 돈을 인출하는데 걸리는 시간
+    }
+}
