3cd2afb0888876488bb6c79ebc98a23e398bee84
jung <wldud2550@naver.com>
2023-10-02 22:17:59 +0900

    [BOJ] 2진수 8진수 / 브론즈1 / 30분

---
+++ src/baekjoon_java/BronzeI/이진수팔진수.java
@@ -0,0 +1,19 @@
+package baekjoon_java.BronzeI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+public class 이진수팔진수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+
+        BigInteger N = new BigInteger(S, 2);
+
+        String result = N.toString(8);
+
+        System.out.println(result);
+    }
+
+}
