556a30e9280521f733957083a8c7f1309e4028cd
jung <wldud2550@naver.com>
2023-10-18 00:09:29 +0900

    [BOJ] 약수 구하기 / 브론즈3 / 10분

---
+++ src/baekjoon_java/Bronze V/약수구하기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 2501번 약수 구하기 (https://www.acmicpc.net/problem/24418)
+ */
+public class 약수구하기 {
+    public static void main(String arg[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());//N의 약수
+        int K = Integer.parseInt(st.nextToken());//K번쨰
+        int result = 0, count = 0;
+
+        for (int i = 1; i <= K; i++) {
+            if (N % i == 0) count++;
+
+            if (count == K) result = i;
+        }
+        System.out.println(result);
+    }
+}
