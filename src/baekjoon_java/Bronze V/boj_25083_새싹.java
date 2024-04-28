a46df9d4cc3a28bb8d68c753d65e4ef2f6db76cb
jung <wldud2550@naver.com>
2024-04-28 21:55:27 +0900

    [BOJ] 새싹 / 브론즈5 / 3m / 17520KB / 200ms

---
+++ src/baekjoon_java/Bronze V/boj_25083_새싹.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class boj_25083_새싹 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
