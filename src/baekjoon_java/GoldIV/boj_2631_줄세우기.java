64ec3beec0e456ca24453ff562bfa9732353172e
jung <wldud2550@naver.com>
2024-05-09 23:01:50 +0900

    [BOJ] 줄 세우기 / 골드4 / 3h / 14084KB / 124ms

---
+++ src/baekjoon_java/GoldIV/boj_2631_줄세우기.java
@@ -0,0 +1,28 @@
+package baekjoon_java.GoldIV;
+
+import java.io.*;
+
+public class boj_2631_줄세우기 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n];
+        int max = 0;기
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n - max);
+    }
+}
