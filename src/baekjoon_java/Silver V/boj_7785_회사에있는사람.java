7c980af6eef5e4303d3ead38f0b8883b7772cc19
jung <wldud2550@naver.com>
2024-10-22 23:11:16 +0900

    [BOJ] 회사에 있는 사람/ 실버5 / 54676KB / 1160ms

---
+++ src/baekjoon_java/Silver V/boj_7785_회사에있는사람.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_7785_회사에있는사람 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        HashMap<String, String> map = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String inout = st.nextToken();
+
+            if(inout.equals("enter"))
+                map.put(name, inout);
+            else if(inout.equals("leave"))
+                map.remove(name);
+        }
+
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList, Collections.reverseOrder());
+
+        for (String key: keyList) {
+            System.out.println(key);
+        }
+    }
+}
