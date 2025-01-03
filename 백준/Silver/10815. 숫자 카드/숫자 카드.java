e10d29827a3a4914e6368d346e0363d31d7aa3fc
jung <wldud2550@naver.com>
2025-01-03 21:57:31 +0900

    [Silver V] Title: 숫자 카드, Time: 2876 ms, Memory: 159136 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arrN = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arrN[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arrN);
+
+        int M = Integer.parseInt(br.readLine());
+        int[] arrM = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            arrM[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            System.out.print(BSearch(arrN, arrM[i]) + " ");
+        }
+    }
+
+    public static int BSearch(int[] arr, int n) {
+
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (arr[mid] < n) left = mid + 1;
+            else if (arr[mid] > n) right = mid - 1;
+            else return 1;
+        }
+        return 0;
+    }
+}
