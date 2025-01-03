5dd9ab8be111bc5ec81eddac6d392e53335fc103
jung <wldud2550@naver.com>
2025-01-03 23:11:49 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -9,32 +9,30 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken()); // 걸그룹 수
         int M = Integer.parseInt(st.nextToken()); // 문제 수
-        String[] quiz = new String[M];
 
         //팀의 이름, 걸그룹의 인원 수, 멤버의 이름
-        // 0 : 팀의 이름 -> 이름 사전 순, 1 : 멤버의 이름 -> 팀 이름
         for (int i = 0; i < N; i++) {
-            String name = br.readLine();
+            String teamName = br.readLine();
             int count = Integer.parseInt(br.readLine());
 
             for (int j = 0; j < count; j++) {
                 String person = br.readLine();
-                map.put(person, name);
+                map.put(person, teamName);
             }
         }
 
         List<String> keySet = new ArrayList<>(map.keySet());
         Collections.sort(keySet);
-
+        // 0 : 팀의 이름 -> 이름 사전 순, 1 : 멤버의 이름 -> 팀 이름
         for (int i = 0; i < M; i++) {
-            quiz[i] = br.readLine();
+            String quiz = br.readLine();
             int flag = Integer.parseInt(br.readLine());
 
             if (flag == 1) {
-                System.out.println(map.get(quiz[i]));
+                System.out.println(map.get(quiz));
             } else if (flag == 0) {
                 for (String key : keySet) {
-                    if (quiz[i].equals(map.get(key)))
+                    if (quiz.equals(map.get(key)))
                         System.out.println(key);
                 }
             }
