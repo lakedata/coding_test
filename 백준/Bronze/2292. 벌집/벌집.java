f69d4124d6df622d34eaab64b62a88d35eb263ca
jung <wldud2550@naver.com>
2025-03-12 00:51:43 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,23 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
-    public static void main(String arg[]) throws IOException {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int count = 1; // 겹 수(최소 루트)
-        int range = 2;    // 범위 (최솟값 기준)
-
-        if (N == 1) {
-            System.out.print(1);
-        } else {
-            while (range <= N) {
-                range = range + (6 * count);
-                count++;
-            }
-            System.out.print(count);
+        int count = 1; 
+        int range = 1; 
+        while (range < n) {
+            range += count * 6;
+            count++;
         }
+        System.out.println(count);
     }
 }
