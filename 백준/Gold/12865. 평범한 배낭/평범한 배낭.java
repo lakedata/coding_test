ec5d65d88f5757d334e592de3140e6dd05d5b627
jung <wldud2550@naver.com>
2025-03-13 02:10:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 53856 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n  = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] weight = new int[n + 1];
+        int[] value = new int[n + 1];
+        int [][]dp = new int[n + 1][k + 1];
+
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] =  Integer.parseInt(st.nextToken());            
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= k; j++) {
+                if(j < weight[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } 
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j - weight[i]] + value[i], dp[i - 1][j]);
+                }
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+}
