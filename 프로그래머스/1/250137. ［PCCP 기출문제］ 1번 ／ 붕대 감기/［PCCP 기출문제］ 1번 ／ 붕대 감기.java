c6d40ba017665cf28bec1cc549c0b79adee4612c
jung <wldud2550@naver.com>
2025-05-15 21:58:22 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,35 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
+        int cnt = bandage[0];
         int answer = health;
-        int attackidx = 0;
-        int count = 0;
-        
-        for(int time = 1; time <= attacks[attacks.length-1][0]; time++) {
-            
-            if(answer <= 0) break; 
-            
-            //공격하는 경우
-            if (attackidx < attacks.length && time == attacks[attackidx][0]) {
-                answer -= attacks[attackidx][1];
-            
-                if (answer <= 0) return -1;
-                count = 0;
-                attackidx++;
+        int std = 0;
+
+        int v1, v2; 
+        for (int[] atk: attacks) {
+            if (answer <= 0) {
+                return -1;
             }
-            //공격하지 않는 경우
-            else {                
-                //초 체력 회복
-                answer += bandage[1];
-                count++;
-                
-                //연속 체력회복
-                if(count == bandage[0]) {              
-                    answer += bandage[2];
-                    count = 0;
-                }
-                if(answer > health) answer = health;
-            }
-        }
-        return answer;
+
+            v1 = atk[0] - std - 1;
+            v2 = v1 / cnt;
+
+            std = atk[0];
+            answer = Math.min(health, answer + (v1 * bandage[1]));
+            answer = Math.min(health, answer + (v2 * bandage[2]));
+            answer -= atk[1];
+        }        
+        return answer <= 0 ? -1 : answer;
     }
 }
