5909a22429c20b4afd26000ef5afbf28a407a767
jung <wldud2550@naver.com>
2025-09-05 17:44:17 +0900

    [level 3] Title: 외벽 점검, Time: 6791.65 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    static final int INF = 987654321;
+    int N, MinCnt;
+    int[] Weak;
+    int[] Dist;
+    
+    void solve(int cnt, int pos, int visited) {
+        if(cnt > Dist.length) return;
+        if(cnt >= MinCnt) return;
+        
+        for(int i = 0; i < Weak.length; ++i) {
+            int nextPos = (pos + i) % Weak.length;
+            int diff = Weak[nextPos] - Weak[pos];
+            
+            if(nextPos < pos)
+                diff += N;
+            
+            if(diff > Dist[Dist.length - cnt])
+                break;
+            
+            visited |= 1 << nextPos;
+        }
+        
+        if(visited == (1 << Weak.length) - 1) {
+            MinCnt = cnt;
+            return;
+        }
+        
+        for(int i = 0; i < Weak.length; ++i) {
+            if((visited & ( 1 << i)) != 0) continue;
+            
+            solve(cnt + 1, i, visited); 
+        }
+    }
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        Arrays.sort(dist);
+        N = n;
+        Weak = weak;
+        Dist = dist;
+        MinCnt = INF;
+        
+        for(int i = 0; i < Weak.length; ++i) {
+            solve(1, i, 0);
+        }
+        
+        if(MinCnt == INF) 
+            return -1;
+        
+        return MinCnt;
+    }
+}
