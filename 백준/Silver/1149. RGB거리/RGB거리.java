b0052287b7f17e861989e82d6fe190b22231e6e2
jung <wldud2550@naver.com>
2025-09-07 22:46:14 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,27 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main{
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); //집의 수
 
-        int n = Integer.parseInt(br.readLine());
-
-        int[][] dp = new int[n + 1][3];
-
-        for (int i = 1; i <= n; i++) {
+        int [][]dp = new int[n + 1][3];
+        for(int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int g = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int red = Integer.parseInt(st.nextToken());
+            int green = Integer.parseInt(st.nextToken());
+            int blue = Integer.parseInt(st.nextToken());
 
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + r;
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + g;
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + b;
+            if(i == 1) {
+                dp[i][0] = red;
+                dp[i][1] = green;
+                dp[i][2] = blue;
+            }
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + red; 
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + green; 
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + blue; 
         }
-        System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
-
+        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
     }
 }
