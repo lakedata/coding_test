c9b95ac90e3f68805add05b3dbce29ccf731b7ce
jung <wldud2550@naver.com>
2025-09-11 21:18:34 +0900

    [Silver III] Title: 달팽이, Time: 680 ms, Memory: 93388 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,48 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-
-        solve(n, c);
-    }
-
-    static void solve(int n, int c) {
-        int[][] arr = new int[n][n];
+class Main {
+    public static void main(String[] args) {
+        int []dx = {-1, 0, 1, 0}; //상 우 하 좌
+        int []dy = {0, 1, 0, -1};
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int find = sc.nextInt();
+        
+        int [][]arr = new int[n][n];
         int value = 1, move = 1;
         int x = n / 2, y = n / 2;
-
-        while (true) {
-            for (int i = 0; i < move; i++) {
-                arr[y--][x] = value++;
+        arr[x][y] = value;
+        
+        while (value < n * n) {
+            for (int i = 0; i < 4 && value < n * n; i++) {
+                for (int m = 0; m < move && value < n * n; m++) {
+                    x += dx[i];
+                    y += dy[i];
+                    arr[x][y] = ++value;
+                }
+                if (i % 2 == 1) move++;   // 두 방향마다 증가
             }
-            if (value == n * n + 1) break;
-
-            for (int i = 0; i < move; i++) {
-                arr[y][x++] = value++;
-            }
-            move++;
-
-            for (int i = 0; i < move; i++) {
-                arr[y++][x] = value++;
-            }
-
-            for (int i = 0; i < move; i++) {
-                arr[y][x--] = value++;
-            }
-            move++;
         }
-
+        
         StringBuilder sb = new StringBuilder();
         int cx = 0, cy = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (c == arr[i][j]) {
+                if (find == arr[i][j]) {
                     cy = i + 1;
                     cx = j + 1;
                 }
@@ -53,5 +41,4 @@ public class Main {
         sb.append(cy + " " + cx);
         System.out.println(sb.toString());
     }
-
 }
