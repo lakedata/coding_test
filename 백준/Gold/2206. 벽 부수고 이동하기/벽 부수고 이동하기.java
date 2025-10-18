d4a091116c8d2ed340554a090f8b7c74dad60492
jung <wldud2550@naver.com>
2025-10-18 09:13:55 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 792 ms, Memory: 150152 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] map;
+    static boolean[][][] visit;
+
+    static class Node {
+        int x, y, dist, wall;
+
+        Node(int x, int y, int dist, int wall) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+            this.wall = wall;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visit = new boolean[N][M][2];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+    }
+
+    public static int bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(x, y, 1, 0));
+        visit[x][y][0] = true;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (node.x == N - 1 && node.y == M - 1) {
+                return node.dist;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (map[nx][ny] == 0 && !visit[nx][ny][node.wall]) {
+                        visit[nx][ny][node.wall] = true;
+                        queue.add(new Node(nx, ny, node.dist + 1, node.wall));
+                    } else if (map[nx][ny] == 1 && node.wall == 0 && !visit[nx][ny][1]) {
+                        visit[nx][ny][1] = true;
+                        queue.add(new Node(nx, ny, node.dist + 1, 1));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
